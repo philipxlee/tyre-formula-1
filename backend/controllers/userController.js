bc815950275702b26345f030fc88afdeb8260beb
Philip Lee <kl445@duke.edu>
2023-05-29 22:22:43 +0800

    Update backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -15,7 +15,29 @@ const authUser = asyncHandler(async (req, res) => {
 // @access Public   
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: "Register User"})
+    const {name, email, password} = req.body;
+    const userExists = await User.findOne({email})
+    if (userExists) {
+        res.status(400);
+        throw new Error("User already exists")
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    }); 
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error("Invalid user data")
+    }
 });
 
 // @desc Logout user
--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,8 @@ import userRoutes from "./routes/userRoutes.js";
 
 connectDB();
 const app = express();
-    
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send("Server ready"));
 app.use(notFound);
